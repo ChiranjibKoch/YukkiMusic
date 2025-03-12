60803b06f8a8320f5b291c4b920ffaa042521d72
Chiranjib Koch <79559271+ChiranjibKoch@users.noreply.github.com>
2025-03-12 15:33:24 +0530

    Update config.py
    Signed-off-by: Chiranjib Koch <79559271+ChiranjibKoch@users.noreply.github.com>

--- config/config.py
+++ config/config.py
@@ -91,10 +91,10 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/TheTeamVivek"
+    "SUPPORT_CHANNEL", "https://t.me/archbots"
 )  # Example:- https://t.me/TheTeamVivek
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/TheTeamVk"
+    "SUPPORT_GROUP", "https://t.me/+VZ5JwtcC_8gyZTFl"
 )  # Example:- https://t.me/TheTeamVk
 
 
